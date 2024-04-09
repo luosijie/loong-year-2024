1ee7237a0c90a85ae9f96fa5d7441e784d1c8d97
luosijie <luo_sj@163.com>
2024-04-09 14:32:54 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import glsl from 'vite-plugin-glsl'
 console.log('----', process.env.NODE_ENV)
 
 export default defineConfig({
-    // base:  process.env.NODE_ENV === 'production' ? '/loong-year-2024/' : '/',
+    base:  process.env.NODE_ENV === 'production' ? '/loong-year-2024/' : '/',
     server: {
         port: 2024
     },
